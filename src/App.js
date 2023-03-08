a444b0849c9412a214cda092dfa6888d2dcbd263
Bj'z <zbjarchi@gmail.com>
2023-03-08 07:01:41 +0200

    :art: Improve structure
    Add Card, change text, center the input

--- src/App.js
+++ src/App.js
@@ -1,78 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import './index.css';
-import backgroundImg from './';
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import backgroundImg from "./img/ART_RGB_CMYK_PANTONE-transformed.webp";
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [colorIndex, setColorIndex] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setColorIndex((prevColorIndex) => (prevColorIndex + 1) % 6);
-    }, 250);
+    }, 150);
 
     return () => clearInterval(intervalId);
   }, []);
 
-  const colors = ['red', 'green', 'blue', 'cyan', 'magenta', 'black'];
+  const colors = ["red", "green", "blue", "cyan", "magenta", "black"];
 
-  const coloredString = inputValue.split('').map((char, index) => (
+  const coloredString = inputValue.split("").map((char, index) => (
     <span key={index} style={{ color: colors[(index + colorIndex) % 6] }}>
       {char}
     </span>
   ));
 
   return (
-
-
-
-
-<div class="card">
-  <div class="tools">
-    <div class="circle">
-      <span class="red box"></span>
-    </div>
-    <div class="circle">
-      <span class="yellow box"></span>
-    </div>
-    <div class="circle">
-      <span class="green box"></span>
-    </div>
-  </div>
-  <div class="card__content">
-  <div id="center">
-      <div>
-        <h3>Make it Blink<span> !</span></h3>
-        <div>
-          <p>You know...</p>
-          <img src={backgroundImg} alt="background image" />
-          <input
-            type="text"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-            placeholder="Enter text here..."
-          />
-          <hr />
-          <h4>{coloredString}</h4>
+    <div class="card">
+      <div class="tools">
+        <div class="circle">
+          <span class="red box"></span>
+        </div>
+        <div class="circle">
+          <span class="yellow box"></span>
+        </div>
+        <div class="circle">
+          <span class="green box"></span>
+        </div>
+      </div>
+      <div class="card__content">
+        <div id="center">
+          <div>
+            <h2>
+              YOU KNOW<span>...</span>
+            </h2>
+            <div>
+              <img src={backgroundImg} alt="background image" id="center" />
+              <h3>
+                So, blink that text<span>!</span> 🚨
+              </h3>
+              <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                placeholder="Enter text here..."
+                id="center"
+              />
+              {/* <hr /> */}
+              <h2>{coloredString}</h2>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
   );
 }
 
